8a34bf4645e6536b56cd1a8316a208766fb63b09
Osama Mohammed Al-zabidi <57198110+omamkaz@users.noreply.github.com>
2024-12-01 05:30:11 +0300

    Delete src directory

--- src/lib/views/card/atypes/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-from .adsl import ADSLCard
-from .lte import LTECard
-from .phone import PhoneCard
--- src/lib/views/list_user/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-from .list_view import UserListView
-from .list_tile import UserListTile
